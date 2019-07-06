6ca5eb36ec3f9dbfd5dbb281eaa568366f15e247
mhk1121992 <mhk1121992@gmail.com>
2019-07-06 15:06:11 +0600

    finished projct. Can upload products and also upvote is working

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField(max_length=1083)
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField(max_length=1200)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_new(self):
+        return self.pub_date.strftime(' %b %e %Y')
+
